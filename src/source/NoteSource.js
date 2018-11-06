fe440c30a41cff5e2aa1fa346def5a905471d5fb
ddody <laccooon@gmail.com>
2018-11-06 20:54:50 +0900

    edit README

--- src/source/NoteSource.js
+++
@@ -1,9 +0,0 @@
-import * as notes from './Notes';
-
-let sounds = {};
-
-for (let i = 0; i < Object.keys(notes).length; i++) {
-  sounds[Object.keys(notes)[i]] = Object.values(notes)[i];
-}
-
-export default sounds;
